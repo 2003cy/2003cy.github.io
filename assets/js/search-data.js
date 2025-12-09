b37f016be96d42cf9b817156038c08d6659d2e77
2003cy <103347489+2003cy@users.noreply.github.com>
2025-12-09 21:48:11 +0000

    Deploying to gh-pages from @ 2003cy/2003cy.github.io@30868d191cc23ec8783053cb7201025bc6294da9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -511,11 +511,25 @@ ninja.data = [{
           window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
         },
       },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/2003cy", "_blank");
+        },
+      },{
         id: 'social-inspire',
         title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0009-0005-5466-5977", "_blank");
         },
       },{
         id: 'social-rss',
